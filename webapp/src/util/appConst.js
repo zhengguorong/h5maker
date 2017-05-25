ad57750977a543979c379b7dad50200ce8b2a519
郑国榕 <zhengguorong@bluemoon.com.cn>
2017-05-25 09:54:29 +0800

    修改bm信息

--- webapp/src/util/appConst.js
+++ webapp/src/util/appConst.js
@@ -1,7 +1,7 @@
 
 let BACKEND_DOMAIN = 'http://localhost:3000'
 if (process.env.NODE_ENV === 'production') {
-  BACKEND_DOMAIN = 'http://h5.limesoftware.cn'
+  BACKEND_DOMAIN = 'http://node.bluemoon.com.cn'
 } else if (process.env.NODE_ENV === 'development') {
   BACKEND_DOMAIN = 'http://localhost:3000'
 }
