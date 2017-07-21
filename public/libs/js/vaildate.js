9b5c8ee60ef3736b18707a7ae48614bb44cb6db2
曾文东 <zengwendong@bluemoon.com.cn>
2017-07-21 14:28:00 +0800

    es6 to es5

--- public/libs/js/vaildate.js
+++ public/libs/js/vaildate.js
@@ -4,7 +4,7 @@
 /**
  * 是否为email，options接收max、min参数
  */
-const isEmail = (value, options) => {
+var isEmail = function(value, options) {
   const rule = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/
   return rule.test(value) && isMatchLength(value, options)
 }
@@ -12,7 +12,7 @@ const isEmail = (value, options) => {
 /**
  * 是否为手机号
  */
-const isMobile = (value) => {
+var isMobile = function(value) {
   const rule = /^(((17[0-9]{1})|(14[0-9]{1})|(15[0-9]{1})|(13[0-9]{1})|(18[0-9]{1}))+\d{8})$/
   return rule.test(value)
 }
@@ -20,7 +20,7 @@ const isMobile = (value) => {
 /**
  * 是否为数字
  */
-const isNum = (value) => {
+var isNum = function(value) {
   const rule = /^[0-9]*$/
   return rule.test(value)
 }
@@ -28,7 +28,7 @@ const isNum = (value) => {
 /**
  * 是否存在表情，options接收max、min参数
  */
-const isEmoji = (substring, options) => {
+var isEmoji = function(substring, options) {
   const rule = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g
   return rule.test(substring) && isMatchLength(substring, options)
 }
@@ -36,14 +36,14 @@ const isEmoji = (substring, options) => {
 /**
  * 是否不为空
  */
-const isNotNull = (value) => {
+var isNotNull = function(value) {
   return value && value.trim() !== ''
 }
 
 /**
  * 是否不为空，options接收max、min参数
  */
-const isInteger = (value, options) => {
+var isInteger = function(value, options) {
   const rule = /^(?:[-+]?(?:0|[1-9][0-9]*))$/
   return rule.test(value) && isMatchLength(value, options)
 }
@@ -51,28 +51,28 @@ const isInteger = (value, options) => {
 /**
  * 是否匹配长度，options接收max、min参数
  */
-const isMatchLength = (value, options) => {
+var isMatchLength = function(value, options) {
   options = options || {}
   let maxCheck = !options.hasOwnProperty('max') || value.length <= Number(options.max)
   let minCheck = !options.hasOwnProperty('min') || value.length >= Number(options.min)
   return maxCheck && minCheck
 }
 
-const isIOS = () => {
+var isIOS = function() {
   var ua = navigator.userAgent.toLowerCase()
   return /iphone|ipad|ipod/.test(ua)
 }
 
-const isAndriod = () => {
+var isAndriod = function() {
   var ua = navigator.userAgent.toLowerCase()
   return /android/.test(ua)
 }
 
-const isMobilePhone = () => {
+var isMobilePhone = function(){
   return isIOS() || isAndriod()
 }
 
-const funDict = {
+var funDict = {
   'phone': 'isMobile',
   'email': 'isEmail',
   'num': 'isNum'
