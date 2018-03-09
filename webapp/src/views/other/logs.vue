2c098587ff1aca0f92e4901e006d83c6f427555c
wesdint <zengwendong@bluemoon.com.cn>
2018-03-09 15:15:32 +0800

    版本日志

--- webapp/src/views/other/logs.vue
+++ webapp/src/views/other/logs.vue
@@ -3,6 +3,12 @@
   <HeaderBar />
   <div class="container">
     <ol>
+      <p>版本：v1.2.0</p>
+      <li>修复相关bug，清理了答卷数据</li>
+      <li>优化了答卷图片上传和删除交互</li>
+      <li>优化了问卷编辑逻辑</li>
+      <li>ps：2018/03/09 16:00 之前的答卷数据已被清除，如需找回，请联系 zengwendong@bluemoon.com.cn</li>
+      <li>加入图片单页功能</li>
       <p>版本：v1.1.0</p>
       <li>加入图片单页功能</li>
       <p>版本：v1.0.0</p>
